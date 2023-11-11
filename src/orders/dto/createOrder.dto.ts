5da144ec12e5a9dfa4a4bcdf108c4c1c555b0e23
temple_19 <tozoaz@gmail.com>
2023-11-11 09:49:40 +0100

    fix order

--- src/orders/dto/createOrder.dto.ts
+++ src/orders/dto/createOrder.dto.ts
@@ -1,4 +1,8 @@
 import { OmitType } from '@nestjs/swagger';
 import { Order } from '../entity/orders.entity';
 
-export class CreateOrderDto extends OmitType(Order, ['id', 'type'] as const) {}
+export class CreateOrderDto extends OmitType(Order, [
+  'id',
+  'type',
+  'user_id',
+] as const) {}
